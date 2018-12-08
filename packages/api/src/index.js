b1b92a549d3dcf43170a9986efda0c9778661f38
Andre Nanninga <ag.nanninga@gmail.com>
2018-12-08 10:10:12 +0100

    travis setup

---
+++ packages/api/src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello world" });
+});
+
+app.listen(port);
+console.log(`Running on port ${port}`);
