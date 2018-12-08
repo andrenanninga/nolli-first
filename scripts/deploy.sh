6697cc4e7f040f15f270b7e8b17d2e4ec60e5b65
Andre Nanninga <ag.nanninga@gmail.com>
2018-12-08 13:16:23 +0100

    fix build script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,5 @@ BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo "current branch: $BRANCH"
 pwd
 
-npm run-script deploy
+now -t $NOW_TOKEN deploy
 now -t $NOW_TOKEN alias "nollie-${BRANCH}.nannin.ga"
