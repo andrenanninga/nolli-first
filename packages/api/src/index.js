c377311e24601dc6533b3d19504690cd796564ee
Andre Nanninga <ag.nanninga@gmail.com>
2018-12-08 14:37:27 +0100

    use @now/node instead of @now/node-server

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -1,16 +1,3 @@
-const express = require("express");
-
-const app = express();
-
-const port = process.env.PORT || 8080;
-
-app.get("/", (req, res) => {
-  res.json({ message: "Hello world" });
-});
-
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello world", api: true });
-});
-
-app.listen(port);
-console.log(`Running on port ${port}`);
+module.exports = (req, res) => {
+  res.end("The time is: " + new Date());
+};
