8ff6fbadadbc0933d8a73e898735b8d802498fa0
Andre Nanninga <ag.nanninga@gmail.com>
2018-12-08 12:57:08 +0100

    now.sh token

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,7 @@ BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo "current branch: $BRANCH"
 pwd
 
-npm run-script deploy
-now alias "nollie-${BRANCH}.nannin.ga"
+printenv
+
+# npm -t run-script deploy
+# now alias "nollie-${BRANCH}.nannin.ga"
