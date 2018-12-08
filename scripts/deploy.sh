4bc9ec4002beec245b8d18cedddaf97fb7fb7b95
Andre Nanninga <ag.nanninga@gmail.com>
2018-12-08 14:31:00 +0100

    try again

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-BRANCH=$(git rev-parse --abbrev-ref HEAD);
-printenv;
+if [[ $TRAVIS_BRANCH ]]; then
+	BRANCH="$TRAVIS_BRANCH";
+else
+	BRANCH=$(git rev-parse --abbrev-ref HEAD);
+fi
 
-echo "current branch: $BRANCH";
+echo "Current branch: $BRANCH";
 
-now -t $NOW_TOKEN deploy;
+now -t "$NOW_TOKEN" deploy;
 
 if [ $BRANCH = "master" ]; then
-	now -t $NOW_TOKEN alias "nolli.nannin.ga";
+	now -t "$NOW_TOKEN" alias "nolli.nannin.ga";
 else
-	now -t $NOW_TOKEN alias "nollie-${BRANCH}.nannin.ga";
+	now -t "$NOW_TOKEN" alias "nollie-$BRANCH.nannin.ga";
 fi
