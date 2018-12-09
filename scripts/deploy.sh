fd59dfcd4b8c4c721ed595c0661849e5a51a3cf6
Andre Nanninga <ag.nanninga@gmail.com>
2018-12-09 16:01:59 +0100

    github status in deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
+function status {
+	GIT_COMMIT=$(git rev-parse HEAD);
+	curl "http://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$GIT_COMMIT?access_token=$GITHUB_ACCESS_TOKEN" \
+		-H "Content-Type: application/json" \
+		-X POST \
+		-d "{\"state\": \"$1\", \"description\": \"Deployment at $ALIAS\", \"target_url\": \"https://$ALIAS\"}"
+}
+
 if [[ $TRAVIS_BRANCH ]]; then
 	BRANCH="$TRAVIS_BRANCH";
 else
 	BRANCH=$(git rev-parse --abbrev-ref HEAD);
 fi
 
-echo "Current branch: $BRANCH";
-
-now -t "$NOW_TOKEN" --no-clipboard --regions=bru deploy;
-
 if [ $BRANCH = "master" ]; then
-	now -t "$NOW_TOKEN" alias "nolli.nannin.ga";
+	ALIAS="nolli.nannin.ga";
 else
-	now -t "$NOW_TOKEN" alias "nollie-$BRANCH.nannin.ga";
+	ALIAS="nolli-$BRANCH.nannin.ga";
 fi
 
-printenv;
+echo "Current branch: $BRANCH, deployment target: $ALIAS";
+status "pending"
+
+now -t "$NOW_TOKEN" --no-clipboard --regions=bru deploy;
+now -t "$NOW_TOKEN" alias "$ALIAS";
+
+status "success"
 
 
 # curl "https://api.github.com/repos/nolli/my_repo/statuses/$GIT_COMMIT?access_token=abc123" \
