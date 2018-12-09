8190da33b44c6914c231c6b640012785ed6aef78
Andre Nanninga <ag.nanninga@gmail.com>
2018-12-09 18:31:34 +0100

    deploy script failure

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
+# Post a 'nolli/deployment' status with the deployment url to the commit/pull request
 function status {
 	GIT_COMMIT=$(git rev-parse HEAD);
-	curl --silent --verbose "https://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$GIT_COMMIT" \
+	curl --silent "https://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$GIT_COMMIT" \
 		-u "fakeyou:$GITHUB_ACCESS_TOKEN" \
 		-H "Content-Type: application/json" \
 		-X POST \
 		-d "{\"context\": \"nolli/deployment\", \"state\": \"$1\", \"description\": \"Deployment at $ALIAS\", \"target_url\": \"https://$ALIAS\"}";
 }
 
-if [[ $TRAVIS_BRANCH ]]; then
-	BRANCH="$TRAVIS_BRANCH";
-else
-	BRANCH=$(git rev-parse --abbrev-ref HEAD);
-fi
 
-if [ $BRANCH = "master" ]; then
-	ALIAS="nolli.nannin.ga";
-else
-	ALIAS="nolli-$BRANCH.nannin.ga";
-fi
+BRANCH="$TRAVIS_BRANCH";
+ALIAS=$([ $BRANCH = "master" ] && echo "nolli.nannin.ga" || echo "nolli-$BRANCH.nannin.ga");
 
 echo "Current branch: $BRANCH, deployment target: $ALIAS";
-status "pending"
+status "pending";
 
-now -t "$NOW_TOKEN" --no-clipboard --regions=bru deploy;
-now -t "$NOW_TOKEN" alias "$ALIAS";
-
-status "success"
+{
+	# Deploy to now.sh
+	now -t "$NOW_TOKEN" --no-clipboard --regions=bru deploy &&
+	now -t "p$NOW_TOKEN" alias "$ALIAS" &&
+	status "success"
+} || {
+	# Deployment has failed
+	status "failure"
+}
