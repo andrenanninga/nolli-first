351976ce7abd03b1b6874150428e3ff59cdb5b31
Andre Nanninga <ag.nanninga@gmail.com>
2018-12-09 18:00:56 +0100

    post deployment status fro commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,8 @@
 
 function status {
 	GIT_COMMIT=$(git rev-parse HEAD);
-	curl "http://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$GIT_COMMIT?access_token=$GITHUB_ACCESS_TOKEN" \
+	curl "http://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$GIT_COMMIT" \
+		-u "fakeyou:$GITHUB_ACCESS_TOKEN" \
 		-H "Content-Type: application/json" \
 		-X POST \
 		-d "{\"state\": \"$1\", \"description\": \"Deployment at $ALIAS\", \"target_url\": \"https://$ALIAS\"}"
@@ -27,9 +28,3 @@ now -t "$NOW_TOKEN" --no-clipboard --regions=bru deploy;
 now -t "$NOW_TOKEN" alias "$ALIAS";
 
 status "success"
-
-
-# curl "https://api.github.com/repos/nolli/my_repo/statuses/$GIT_COMMIT?access_token=abc123" \
-#   -H "Content-Type: application/json" \
-#   -X POST \
-#   -d "{\"state\": \"$STATUS\", \"description\": \"Jenkins\", \"target_url\": \"http://my.jenkins.box.com/job/dividata/$BUILD_NUMBER/console\"}"
