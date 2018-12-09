710ca2256d124adec0a22fc7ca792508bb1a2dc5
Andre Nanninga <ag.nanninga@gmail.com>
2018-12-09 19:06:46 +0100

    travis setup (#8)
    - Travis setup
    - Now.sh configuration
    - Web & api package setup

---
+++ scripts/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Post a 'nolli/deployment' status with the deployment url to the commit/pull request
+function status {
+	GIT_COMMIT=$(git rev-parse HEAD);
+	curl --silent "https://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$GIT_COMMIT" \
+		-u "fakeyou:$GITHUB_ACCESS_TOKEN" \
+		-H "Content-Type: application/json" \
+		-X POST \
+		-d "{\"context\": \"nolli/deployment\", \"state\": \"$1\", \"description\": \"Deployment at $ALIAS\", \"target_url\": \"https://$ALIAS\"}";
+}
+
+BRANCH="$TRAVIS_BRANCH";
+ALIAS=$([ $BRANCH = "master" ] && echo "nolli.nannin.ga" || echo "nolli-$BRANCH.nannin.ga");
+
+echo "Current branch: $BRANCH, deployment target: $ALIAS";
+status "pending";
+
+{
+	# Deploy to now.sh
+	now -t "$NOW_TOKEN" --no-clipboard --regions=bru deploy &&
+	now -t "$NOW_TOKEN" alias "$ALIAS" &&
+	status "success"
+} || {
+	# Deployment has failed
+	status "failure"
+	exit 1
+}
